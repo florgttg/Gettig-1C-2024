a74a908db8beaf7b1e182b603e2abd2b559d54ee
florgttg <gettigflor@gmail.com>
2024-06-10 10:29:10 -0300

    Se prueba acelerometro

---
+++ firmware/drivers/devices/src/ADXL335.c
@@ -0,0 +1,157 @@
+/** @brief driver que maneja la lectura de datos obtenida a partir de un acelerómetro.
+ **
+ **/
+
+/*
+ * Initials     Name
+ * ---------------------------
+ *
+ */
+
+/*
+ * modification history (new versions first)
+ * -----------------------------------------------------------
+ * 20210609 v0.0.1 initials initial version
+ */
+
+/*==================[inclusions]=============================================*/
+#include "ADXL335.h"
+
+/*==================[macros and definitions]=================================*/
+/** @def MAX_VOLTAGE
+ * @brief Máximo valor de voltaje en mV que puede medir el driver
+ */
+#define MAX_VOLTAGE 3300
+/** @def MAX_VALUE
+ * @brief Máximo valor de bits
+ */
+#define MAX_VALUE 1024
+/** @def OFFSET
+ * @brief Mitad del valor máximo de voltaje correspondiente en bits
+ */
+#define OFFSET 3300.0/2.0
+/** @def SENSITIVITY
+ * @brief Valor de la sensibilidad dado por el driver
+ */
+#define SENSITIVITY 300.0
+
+/*==================[internal data declaration]==============================*/
+
+bool conversion; /**< booleando si realiza o no la conversión*/
+uint16_t valor; /**< Valor convertido*/
+uint8_t canal; /**< Canal de la placa*/
+
+/*==================[internal functions declaration]=========================*/
+
+/**@fn float UnitConvert(uint16_t value)
+ * @brief  Función que convierte el valor leído en unidades de gravedad
+ * @return valor convertido
+ */
+float UnitConvert(uint16_t value, float offset);
+
+/*==================[internal data definition]===============================*/
+
+float offset_x;
+float offset_y;
+float offset_z;
+/*==================[external data definition]===============================*/
+
+analog_input_config_t my_ad_x = {CH1,ADC_SINGLE, 0}; /**< Typedef configuración puerto analógico/digital canal x*/
+analog_input_config_t my_ad_y = {CH2,ADC_SINGLE, 0}; /**< Typedef configuración puerto analógico/digital canal y*/
+analog_input_config_t my_ad_z = {CH3,ADC_SINGLE, 0}; /**< Typedef configuración puerto analógico/digital canal z*/
+
+/*==================[internal functions definition]==========================*/
+
+
+
+float UnitConvert(uint16_t value, float offset){
+
+	float new_value;
+
+	new_value = value - offset;
+	//new_value = new_value/SENSITIVITY;
+
+	return new_value;
+}
+
+/*==================[external functions definition]==========================*/
+
+bool ADXL335Init(){
+    AnalogInputInit(&my_ad_x);
+    AnalogInputInit(&my_ad_y);
+    AnalogInputInit(&my_ad_z);
+	offset_x = OFFSET;
+	offset_y = OFFSET;
+	offset_z = OFFSET;
+	return 1;
+}
+
+int ReadXValueInt(){
+	AnalogInputReadSingle(my_ad_x.input, &valor);
+	return valor;
+}
+
+float ReadXValue(){
+	AnalogInputReadSingle(my_ad_x.input, &valor);
+	return UnitConvert(valor, offset_x);
+}
+
+int ReadYValueInt(){
+    AnalogInputReadSingle(my_ad_y.input, &valor);
+	return valor;
+}
+
+float ReadYValue(){
+	AnalogInputReadSingle(my_ad_y.input, &valor);
+	return UnitConvert(valor, offset_y);
+}
+
+int ReadZValueInt(){
+    AnalogInputReadSingle(my_ad_z.input, &valor);
+	return valor*3.6; /* Resistor divider for HCSR-04 */
+}
+
+float ReadZValue(){
+	AnalogInputReadSingle(my_ad_z.input, &valor);
+	return UnitConvert(valor*3.6, offset_z); /* Resistor divider for HCSR-04 */
+}
+
+float GetXOffset(){
+	return offset_x;
+}
+
+float GetYOffset(){
+	return offset_y;
+}
+
+float GetZOffset(){
+	return offset_z;
+}
+
+bool ADXL335Calibration(){
+	
+	offset_x = 0;
+	offset_y = 0;
+	offset_z = 0;
+	int i=0;
+	for(i=0;i<10;i++){
+		offset_x += ReadXValueInt();
+		offset_y += ReadYValueInt();
+		offset_z += ReadZValueInt()-SENSITIVITY;
+	}
+    offset_x=offset_x/10;
+    offset_y=offset_y/10;
+	offset_z=offset_z/10;
+	return 1;
+}
+
+bool ADXL335DeInit(gpio_t gSelect1, gpio_t gSelect2){
+
+	return 1;
+}
+
+
+/** @} doxygen end group definition */
+/** @} doxygen end group definition */
+/** @} doxygen end group definition */
+/*==================[end of file]============================================*/
--- firmware/projects/proyecto_integrador/main/proyecto_integrador.c
+++ firmware/projects/proyecto_integrador/main/proyecto_integrador.c
@@ -1,4 +1,4 @@
-/*! @mainpage Template
+/*! @mainpage Proyecto Integrador
  *
  * @section genDesc General Description
  *
@@ -15,11 +15,11 @@
  *
  * @section changelog Changelog
  *
- * |   Date	    | Description                                    |
- * |:----------:|:-----------------------------------------------|
- * | 17/05/2024 | Se pone emulador bluetooth HID para simular teclado|		                         |
+ * |   Date	    | Description                                        |
+ * |:----------:|:---------------------------------------------------|
+ * | 17/05/2024 | Se pone emulador bluetooth HID para simular teclado|
  *
- * @author Albano Peñalva (albano.penalva@uner.edu.ar)
+ * @author Florencia Gettig (florenci.gettig@ingenieria.uner.edu.ar)
  *
  */
 
@@ -35,26 +35,38 @@
 #include "led.h"
 #include "ble_hid_mcu.h"
 #include "switch.h"
-//#include "analog_io_mcu.h"
+#include "ADXL335.h"
 /*==================[macros and definitions]=================================*/
 #define CONFIG_BLINK_PERIOD 500
-#define DELAY_MEASURE 50
+#define DELAY_MEASURE 400
 #define LED_BT LED_1
+
 /*==================[internal data definition]===============================*/
 bool click = false;
+TaskHandle_t acel_task_handle = NULL;
 
 /*==================[internal functions declaration]=========================*/
 /**
- * @brief
+ * @brief Función que envía un comando de teclado de la tecla espacio a través de Bluetooth.
+ *
+ */
+void FuncTecla0(void)
+{
+	// keyboard_cmd_t space = HID_KEY_SPACEBAR;
+	// BleHidSendKeyboard(0, &space, 1);
+	ADXL335Calibration();
+}
+/**
+ * @brief Función que envía un comando de teclado de la tecla flecha arriba a través de Bluetooth.
  *
  */
 void FuncTecla1(void)
 {
-	keyboard_cmd_t space = HID_KEY_SPACEBAR;
-	BleHidSendKeyboard(0, &space, 1);
+	keyboard_cmd_t up = HID_KEY_UP_ARROW;
+	BleHidSendKeyboard(0, &up, 1);
 }
 /**
- * @brief
+ * @brief Función que envía un comando de teclado de la tecla flecha abajo a través de Bluetooth.
  *
  */
 void FuncTecla2(void)
@@ -62,15 +74,51 @@ void FuncTecla2(void)
 	keyboard_cmd_t down = HID_KEY_DOWN_ARROW;
 	BleHidSendKeyboard(0, &down, 1);
 }
+void AcelerometroTask(void *pvParameter)
+{
+	float acel_x0 = 0, acel_y0 = 0, acel_z0 = 0;
+	float acel_x, acel_y, acel_z;
+	float umbral_y = 100;
+	while (1)
+	{
+		// if (BleHidStatus() == BLE_CONNECTED)
+		{
+			acel_x = ReadXValue();
+			acel_y = ReadYValue();
+			acel_z = ReadZValue();
+
+			if ((acel_y - acel_y0) > umbral_y)
+			{
+				FuncTecla2();
+				acel_y0 = acel_y;
+			}
+			else if ((acel_y0 - acel_y) > umbral_y)
+{
+				FuncTecla1();
+			acel_y0 = acel_y;
+		}
+		else(acel_y0 = acel_y);
+		printf("el valor de x %f,y %f,z %f   \n", acel_x, acel_y, acel_z);
+	}
+	vTaskDelay(DELAY_MEASURE / portTICK_PERIOD_MS);
+}
+}
+
 /*==================[external functions definition]==========================*/
 void app_main(void)
 {
 	LedsInit();
+	// Teclas
 	SwitchesInit();
-	SwitchActivInt(SWITCH_1, FuncTecla1, 0);
+	SwitchActivInt(SWITCH_1, FuncTecla0, 0);
 	SwitchActivInt(SWITCH_2, FuncTecla2, 0);
+	// Inicializa bluetooth
 	BleHidInit("EP_HID");
 
+	ADXL335Init();
+	xTaskCreate(&AcelerometroTask, "ACELEROMETRO", 4096, NULL, 5, &acel_task_handle);
+
+	// Chequea que el bluetooth este correctamente conectado con el led1
 	while (1)
 	{
 		vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
