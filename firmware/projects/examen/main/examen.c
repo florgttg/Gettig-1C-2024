7a33993c6a6391826e44aedee0f921bb2c7827f6
florgttg <gettigflor@gmail.com>
2024-06-11 11:29:43 -0300

    Se crean tareas y se le da el tiempo con timers para que se ejecuten cada 3 segundos

---
+++ firmware/projects/examen/main/examen.c
@@ -0,0 +1,41 @@
+/*! @mainpage Examen:irrigación automática de plantas
+
+ *
+ * @section genDesc General Description
+ *
+ * Este proyecto trata de un  dispositivo basado en la ESP-EDU que permite 
+ * controlar el riego y el pH de una plantera.
+ *
+ * 
+ *
+ * @section hardConn Hardware Connection
+ *
+ * |    Peripheral  |   ESP32   	|
+ * |:--------------:|:--------------|
+ * | 	PIN_X	 	| 	GPIO_X		|
+ *
+ *
+ * @section changelog Changelog
+ *
+ * |   Date	    | Description                                    |
+ * |:----------:|:-----------------------------------------------|
+ * | 11/06/2024 | Document creation		                         |
+ *
+ * @author Florencia Gettig
+ *
+ */
+
+/*==================[inclusions]=============================================*/
+#include <stdio.h>
+#include <stdint.h>
+/*==================[macros and definitions]=================================*/
+
+/*==================[internal data definition]===============================*/
+
+/*==================[internal functions declaration]=========================*/
+
+/*==================[external functions definition]==========================*/
+void app_main(void){
+	printf("Hello world!\n");
+}
+/*==================[end of file]============================================*/
